0d686c4bd7943b960a5d7df8575ed2e184c9958a
anasnadkar45 <anasnadkar23@gmail.com>
2024-09-06 07:10:00 +0530

    Created route for platform

--- src/components/global/Home/PlatformCard.tsx
+++ src/components/global/Home/PlatformCard.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 interface platformProps {
@@ -12,8 +13,8 @@ interface platformProps {
 
 const PlatformCard = ({ platform }: platformProps) => {
     return (
-        <div>
-            <div key={platform.id} className='bg-card p-3 relative flex flex-col justify-center min-h-[200px] border-2 rounded-b-lg rounded-r-lg'>
+        <Link href={`/home/${platform.id}`}>
+            <div key={platform.id} className='bg-card p-3 hover:cursor-pointer relative flex flex-col justify-center min-h-[200px] border-2 rounded-b-lg rounded-r-lg'>
                 <div className='w-full flex justify-center'>
                     <Image src={platform.logo} width={150} height={150} alt={platform.name} />
                 </div>
@@ -21,7 +22,7 @@ const PlatformCard = ({ platform }: platformProps) => {
                 <p className='text-muted-foreground'>Size: 20</p>
                 <div className='absolute bg-card border-t-[14px] border-r-2 border-l-2 w-[60%] h-10 -top-10 -left-[1.5px] rounded-t-2xl'></div>
             </div>
-        </div>
+        </Link>
     )
 }
 
